09c6c081208f80bf0500fbea286070a04d9afbc8
Ahsas Sharma <sharma.ahsas@gmail.com>
2019-08-04 11:24:51 +0530

    Add image upload to S3 feature

--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -4,9 +4,14 @@ import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({
+	signatureVersion: 'v4'
+})
 
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = Number(process.env.SIGNED_URL_EXPIRATION)
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 	console.log('Caller event', event)
@@ -27,19 +32,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   // TODO: Create an image
 	const imageId = uuid.v4()
-	const parsedBody = JSON.parse(event.body)
-
-	const newItem = {
-		id: imageId,
-		groupId: groupId,
-		timestamp: String(Math.floor(Date.now())),
-		...parsedBody
-	}
-
-	await docClient.put({
-		TableName: imagesTable,
-		Item: newItem
-	}).promise()
+	const newItem = await createNewImage(imageId, groupId, event)
+	const uploadUrl = getUploadUrl(imageId)
 
   return {
     statusCode: 201,
@@ -47,9 +41,38 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({ 
-			newItem
+			newItem,
+			uploadUrl
 		})
   }
+}
+
+// HELPER
+async function createNewImage(imageId: string, groupId: string, event: any) {
+	const parsedBody = JSON.parse(event.body)
+
+	const newItem = {
+		id: imageId,
+		groupId: groupId,
+		timestamp: String(Math.floor(Date.now())),
+		...parsedBody,
+		imageUrl: 'https://${bucketName}.s3.amazonaws.com/${imageId}'
+	}
+
+	await docClient.put({
+		TableName: imagesTable,
+		Item: newItem
+	}).promise()
+
+	return newItem
+}
+
+function getUploadUrl(imageId: string){
+	return s3.getSignedUrl('putObject',{
+		Bucket: bucketName,
+		Key: imageId,
+		Expires: urlExpiration
+	})
 }
 
 async function groupExists(groupId: string) {
